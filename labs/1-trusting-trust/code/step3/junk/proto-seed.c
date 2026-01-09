fc64a384081a3fb20f17703b2df2e21ea36dc0da
Colin Skinner <colinaskinner@gmail.com>
2026-01-09 03:33:57 -0800

    lab 2 step3!

--- labs/1-trusting-trust/code/step3/junk/proto-seed.c
+++
@@ -1,9 +0,0 @@
-int i;
-
-// Q: why can't we just print prog twice?
-printf("char prog[] = {\n");
-for(i = 0; prog[i]; i++)
-    printf("\t%d,%c", prog[i], (i+1)%8==0 ? '\n' : ' ');
-printf("0 };\n");
-printf("%s", prog);
-return 0;
--- labs/1-trusting-trust/code/step3/make-trojan-compiler.sh
+++ labs/1-trusting-trust/code/step3/make-trojan-compiler.sh
@@ -1,33 +1,15 @@
-# Want to make attack.c into an array in some file
 cd ../step1
 make check
 cd ../step3
-# gcc attack-arr-gen.c -o attack-arr-gen # the payload we want to keep replicating
-# ./attack-arr-gen < payload.c > payload-bytes.c
-
-# sed -i '' 's/prog/payload/g' payload-bytes.c
-# cat payload-bytes.c proto-seed.c > attack-seed.c
-# cat payload-bytes.c payload.c > attack-seed.c
-cat payload.c > attack-seed.c
-
-# manually put payload-bytes.c array at the beginning of the seed
 
 ../step1/quine-gen < attack-seed.c > attack-quine.c # makes bytes out of trojan-compiler
 
 gcc -Og -g -Wall trojan-compiler2.c -o trojan-compiler2
 
-./trojan-compiler2 ../step2/compiler.c -o compiler # FINALLY DID IT
+# make clean
+# rm -f compiler login
 
-./compiler ../step2/compiler.c -o compiler
-./compiler ../step2/compiler.c -o compiler
-./compiler ../step2/compiler.c -o compiler
-./compiler ../step2/compiler.c -o compiler
-./compiler ../step2/login.c -o login
-
-
-printf "ken\n" | ./login
-printf "guest\npassword\n" | ./login
-
+# echo "PASSES (Mac does the weird thing where it is not equal if I do the check.\nMaybe running in UTM VM will help)"
 # ./cc-attacked ../step2/compiler.c -o cc-attacked
 # ./cc-attacked ../step2/compiler.c -o cc-attacked
 # ./cc-attacked ../step2/compiler.c -o cc-attacked
