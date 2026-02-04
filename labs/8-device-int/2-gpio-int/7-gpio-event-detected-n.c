1f1f16760ae50c2d188cdb191032acffefebb6b8
engler <ddd.rrr.eee@gmail.com>
2026-02-03 21:24:12 -0800

    minor

--- labs/8-device-int/2-gpio-int/7-gpio-event-detected-n.c
+++ labs/8-device-int/2-gpio-int/7-gpio-event-detected-n.c
@@ -1,6 +1,6 @@
 #include "rpi.h"
 
 void notmain(void) {
-    for(int i = 0; i < 34; i++)
+    for(int i = 0; i < 32; i++)
         trace("event detected = %d\n", gpio_event_detected(i));
 }
--- labs/8-device-int/2-gpio-int/7-gpio-set-rising-n.c
+++ labs/8-device-int/2-gpio-int/7-gpio-set-rising-n.c
@@ -1,6 +1,6 @@
 #include "rpi.h"
 
 void notmain(void) {
-    for(int i = 0; i < 34; i++)
+    for(int i = 0; i < 32; i++)
         gpio_int_rising_edge(i);
 }
