6f1515ddc2fcc81229a7c681d88c65207a2993a7
engler <ddd.rrr.eee@gmail.com>
2026-02-05 16:50:51 -0800

    minor

--- libpi/staff-src/default-handler-undefined-inst.c
+++ libpi/staff-src/default-handler-undefined-inst.c
@@ -1,4 +1,4 @@
 #include "rpi.h"
 void undefined_instruction_vector(unsigned pc) { 
-    panic("unhandled undefined inst exception: pc=%x\n", pc);
+    panic("unhandled undefined inst exception: pc=%x [inst=%x]\n", pc, *(uint32_t*)pc);
 }
