cc291f391decaf0b3e71e594cff8a5887aaf4820
Colin Skinner <colinaskinner@gmail.com>
2026-02-14 18:26:19 -0800

    breakpoint checkpoint

--- labs/11-debug-hw/code/breakpoint.c
+++ labs/11-debug-hw/code/breakpoint.c
@@ -18,6 +18,37 @@ void brkpt_match_init(void) {
 // (bcr1 p13-17 and bvr1 p13-16) so dont't conflict 
 // with single-stepping.   
 void brkpt_match_set(uint32_t addr) {
+    
+    // 0. Enable CP14
+    brkpt_match_init();
+    
+    // 1. Read the BCR1.
+    uint32_t bcr1 = cp14_bcr1_get();
+
+    // 2. Clear the BCR1[0] enable watchpoint bit in the read word 
+    //    and write it back to BCR1. Now the watchpoint is disabled.
+    bcr1 &= ~1;
+    cp14_bcr1_set(bcr1);
+    
+    // 3. Write the DMVA to the BVR1 register.
+    cp14_bvr1_set(addr);
+    
+    
+    
+    // 4. Write to the BCR1 with its fields set as follows:
+    bcr1 = cp14_bcr1_get();
+
+    bcr1 &= ~(1 << 20);          // BCR1[20] enable linking bit cleared
+    bcr1 &= ~(0b11 << 14);       // BCR1[15:14] Matches in secure and non-secure
+    bcr1 |= 0b1111 << 5;         // BCR1[8:5] Byte address select (+1-3 offsets also trigger)
+    // No load/store access stuff because it is instruction fault and not data fault
+    bcr1 |= 0b11 << 1;           // BCR1[2:1] Supervisor access (either privileged or user)
+    bcr1 |= 0b1;                 // BCR1[0] enable breakpoint bit set
+
+    cp14_bcr1_set(bcr1);
+
+    prefetch_flush();
+
     staff_brkpt_match_set(addr);
 }
 
@@ -33,6 +64,7 @@ void brkpt_match_stop(void) {
 
 // return the match addr (bvr1)
 uint32_t brkpt_match_get(void) {
+    // return cp14_bvr1_get();
     return staff_brkpt_match_get();
 }
 
@@ -42,13 +74,51 @@ uint32_t brkpt_match_get(void) {
 // for simplicity: for matching we use breakpoint 0. 
 // so set bcr0 and bvr0.
 void brkpt_mismatch_set(uint32_t addr) {
-    staff_brkpt_mismatch_set(addr);
+
+    // 0. Enable CP14
+    brkpt_match_init();
+    
+    // 1. Read the BCR0.
+    uint32_t bcr0 = cp14_bcr0_get();
+
+    // 2. Clear the BCR0[0] enable watchpoint bit in the read word 
+    //    and write it back to BCR0. Now the watchpoint is disabled.
+    bcr0 &= ~1;
+    cp14_bcr0_set(bcr0);
+    
+    // 3. Write the DMVA to the BVR1 register.
+    cp14_bvr1_set(addr);
+    
+    
+    
+    // 4. Write to the BCR0 with its fields set as follows:
+    bcr0 = cp14_bcr0_get();
+
+    bcr0 &= ~(1 << 20);          // BCR0[20] enable linking bit cleared
+    bcr0 &= ~(0b11 << 14);       // BCR0[15:14] Matches in secure and non-secure
+    bcr0 |= 0b1111 << 5;         // BCR0[8:5] Byte address select (+1-3 offsets also trigger)
+    // No load/store access stuff because it is instruction fault and not data fault
+    bcr0 |= 0b11 << 1;           // BCR0[2:1] Supervisor access (either privileged or user)
+    bcr0 |= 0b1;                 // BCR0[0] enable breakpoint bit set
+
+    cp14_bcr0_set(bcr0);
+
+    prefetch_flush();
+    
+    // staff_brkpt_match_set(addr);
+    // staff_brkpt_mismatch_set(addr);
 }
 
 // this will mismatch on the first instruction at user level.
 void brkpt_mismatch_start(void) {
     // 1. check DSCR: if not enabled, enable it.
+    brkpt_match_init();
+    
     // 2. set brkpt_mismatch_set(0)
+    
+    
+
+
     staff_brkpt_mismatch_start();
 }
 
